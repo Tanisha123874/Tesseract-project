44517cddd8b0fe1ac40594063ea7b8a044f58ffe
tanishaxtyagi@gmail.com <tanishaxtyagi@gmail.com>
2023-03-31 04:26:19 +0000

    fix:tesseract version error issue resolved

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+const { createWorker } = require("tesseract.js");
+async function extractTextFromImage(imagePath) {
+  const worker = createWorker({ spawnWorker: true });
+  await worker.load();
+  await worker.loadLanguage("eng");
+  await worker.initialize("eng");
+  const {
+    data: { text },
+  } = await worker.recognize(imagePath);
+  await worker.terminate();
+  return text;
+}
+extractTextFromImage("public/image.jpg")
+  .then((text) => {
+    text.split("\n").forEach((element) => {
+      console.log(element);
+    });
+  })
+  .catch((error) => console.error(error));
